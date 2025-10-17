55a934b68fad2d3694ae6d77dac60fbe1e375d15
rene-buikman <rene.buikman@qonqord.com>
2025-10-17 13:02:50 +0200

    change default string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Fix for MySQL key length issue
+        Schema::defaultStringLength(191);
+        
         Vite::prefetch(concurrency: 3);
 
         DB::listen(function ($query) {
